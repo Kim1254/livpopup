3c47537cc058fcfb2e50db7b692379538b44ee90
KyeongMin <93812148+KyeongMin5307@users.noreply.github.com>
2022-10-24 11:43:50 +0900

    Object addition

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Living Popup book
-A WebGL website that displaying a animated popup book
+A WebGL website for displaying a animated popup book
 
 Libraries Used:
 - [Three.js](https://github.com/mrdoob/three.js/)
 - [FOLD](https://github.com/edemaine/fold)
 
-Contributor:
+Contributors:
 - [Kim Kyeongmin](https://github.com/KyeongMin5307)
-- [Yang suyeon](https://github.com/#)
-- [Jeong yeongjun](https://github.com/#)
-- [Hong Yungi](https://github.com/#)
+- [Yang suyeon](https://github.com/salmonsalad)
+- [Jeong yeongjun](https://github.com/nojeck)
+- [Hong Yungi](https://github.com/dbsrl1026)
 
 Requirement:
 - Java Development Kit 8
--- src/main/resources/static/js/render.js
+++ src/main/resources/static/js/render.js
@@ -9,7 +9,7 @@ window.onload = function init()
 	renderer.setSize(canvas.width,canvas.height);
 
 	const scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xffffff);
+	scene.background = new THREE.Color(0x000000);
 
 	var camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.01, 1000);
 	camera.rotation.y = 0.25 * Math.PI;
@@ -19,30 +19,29 @@ window.onload = function init()
 
 	const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	hlight = new THREE.AmbientLight (0x404040,50);
+	hlight = new THREE.AmbientLight (0x404040,1);
 	scene.add(hlight);
-	light = new THREE.PointLight(0xc4c4c4,10);
+	light = new THREE.PointLight(0xc4c4c4,1);
 	light.position.set(0,3000,5000);
 	scene.add(light);
 
-	light2 = new THREE.PointLight(0xc4c4c4,10);
+	light2 = new THREE.PointLight(0xc4c4c4,1);
 	light2.position.set(5000,1000,0);
 	scene.add(light2);
 
-	light3 = new THREE.PointLight(0xc4c4c4,10);
+	light3 = new THREE.PointLight(0xc4c4c4,1);
 	light3.position.set(0,1000,-5000);
 	scene.add(light3);
 
-	light4 = new THREE.PointLight(0xc4c4c4,10);
+	light4 = new THREE.PointLight(0xc4c4c4,1);
 	light4.position.set(-5000,3000,5000);
 	scene.add(light4);
 
 	hlight = new THREE.AmbientLight(0xffffff, 2);
 	scene.add(hlight);
 	
-	// Reference: "Scissor" (https://skfb.ly/69M6S) by Francesco Coldesina is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 	const loader = new THREE.GLTFLoader();
-	loader.load('./gltf/scene.gltf', function(gltf) {
+	loader.load('./gltf/book/scene.gltf', function(gltf) {
 		model = gltf.scene.children[0];
 		model.scale.set(1.0,1.0,1.0);
 		scene.add(gltf.scene);
