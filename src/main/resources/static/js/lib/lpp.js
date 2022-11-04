a105ffde6b5b9e87f0e4b858b3e4c4ed0aa52e0a
KyeongMin <93812148+KyeongMin5307@users.noreply.github.com>
2022-11-05 03:58:13 +0900

    Configuration parser

---
+++ src/main/resources/static/js/lib/lpp.js
@@ -0,0 +1,114 @@
+var LPP = (function () {
+	'use strict';
+	
+	var _utils = {};
+	
+	// public
+	
+	_utils.Parser = class ConfigParser {
+		constructor(filepath)
+		{
+			this._path = filepath;
+			this._status = 0;
+			
+			this._model = [];
+		}
+		
+		open(callback = undefined)
+		{
+			if (xhr != undefined)
+			{
+				xhr.open('get', this._path, true);
+				xhr.onreadystatechange = function()
+				{
+					if (xhr.readyState != 4)
+						return;
+					
+					if (xhr.status != 200)
+					{
+						console.log("Error:", xhr.readyState, xhr.status);
+						this._status = -1;
+					}
+					
+					var data = xhr.responseText;
+					var json = JSON.parse(data);
+					
+					this._data = json;
+					this._status = 2;
+					
+					this._bg = parseInt(json['background'], 16);
+					
+					this._light = [];
+					
+					var light = json['light'];
+					if (light != undefined)
+					{
+						var keys = Object.keys(light);
+						for (var i = 0; i < keys.length; i++)
+						{
+							var light_elem = light[keys[i]];
+							var elem = {
+								type: light_elem['type'],
+								color: parseInt(light_elem['color'], 16),
+								position: light_elem['position'],
+								additive: light_elem['additive']
+							};
+							this._light.push(elem);
+						}
+					}
+					
+					this._model = [];
+					
+					var model = json['model'];
+					if (model != undefined)
+					{
+						var keys = Object.keys(model);
+						for (var i = 0; i < keys.length; i++)
+						{
+							var model_elem = model[keys[i]];
+							var elem = {
+								path: model_elem['path'],
+								scale: model_elem['scale'],
+								position: model_elem['position'],
+								animation: model_elem['animation']
+							};
+							this._model.push(elem);
+						}
+					}
+					
+					if (callback != undefined)
+						callback(xhr);
+					
+					console.log(this._model);
+				}
+				
+				xhr.send(null);
+				this._status = 1;
+			}
+		}
+		
+		get getData()
+		{
+			return this._data;
+		}
+	}
+	
+	return init();
+	
+	// protected
+	
+	function init()
+	{
+		// Reference: https://cross-milestone.tistory.com/148
+		if (window.ActiveObject)
+			xhr = new ActiveXObject("Msxml2.XMLHTTP");
+		else if (window.XMLHttpRequest)
+			xhr = new XMLHttpRequest();
+		
+		return _utils;
+	}
+	
+	// private
+	var xhr = undefined;
+	
+})();
--- src/main/resources/static/js/render.js
+++ src/main/resources/static/js/render.js
@@ -1,49 +1,64 @@
+FOLD = require('fold');
+window.addEventListener('resize', resizeEvent);
+
+var canvas;
+
+var renderer;
+var camera;
+var controls;
+
+var repaint = false;
+
+var timer_list = [];
 
 window.onload = function init()
 {
-	const canvas = document.getElementById( "gl-canvas" );
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas = document.getElementById("gl-canvas");
+	
+	var parser = new LPP.Parser("./tales/book1/config.json");
+	parser.open();
 
-	const renderer = new THREE.WebGLRenderer({canvas});
-	renderer.setSize(canvas.width,canvas.height);
+	resizeEvent();
 
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color(0x000000);
+	
+	var li = 1;
 
-	var camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.01, 1000);
-	camera.rotation.y = 0.25 * Math.PI;
-	camera.position.x = 15;
-	camera.position.y = 15;
-	camera.position.z = 15;
-
-	const controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-	hlight = new THREE.AmbientLight (0x404040,1);
+	hlight = new THREE.AmbientLight(0x404040, li);
 	scene.add(hlight);
-	light = new THREE.PointLight(0xc4c4c4,1);
+	
+	light = new THREE.PointLight(0xc4c4c4, li);
 	light.position.set(0,3000,5000);
 	scene.add(light);
 
-	light2 = new THREE.PointLight(0xc4c4c4,1);
+	light2 = new THREE.PointLight(0xc4c4c4, li);
 	light2.position.set(5000,1000,0);
 	scene.add(light2);
 
-	light3 = new THREE.PointLight(0xc4c4c4,1);
+	light3 = new THREE.PointLight(0xc4c4c4, li);
 	light3.position.set(0,1000,-5000);
 	scene.add(light3);
 
-	light4 = new THREE.PointLight(0xc4c4c4,1);
+	light4 = new THREE.PointLight(0xc4c4c4, li);
 	light4.position.set(-5000,3000,5000);
 	scene.add(light4);
-
-	hlight = new THREE.AmbientLight(0xffffff, 2);
-	scene.add(hlight);
+	
+	const clock = new THREE.Clock();
 	
 	const loader = new THREE.GLTFLoader();
 	loader.load('./gltf/book/scene.gltf', function(gltf) {
 		model = gltf.scene.children[0];
-		model.scale.set(1.0,1.0,1.0);
+		model.scale.set(1.0, 1.0, 1.0);
+		
+		mixer = new THREE.AnimationMixer(gltf.scene);
+		
+		const clip = THREE.AnimationClip.findByName(gltf.animations, 'BookOpen');
+		const act = mixer.clipAction(clip);
+		
+		act.setLoop(0, 2);
+		act.play();
+		
 		scene.add(gltf.scene);
 		animate();
 	}, undefined, function (error) {
@@ -54,6 +69,33 @@ window.onload = function init()
 		// rotate along with X-axis
 		renderer.render(scene, camera);
 		requestAnimationFrame(animate);
+		
+		controls.update();
+		
+		mixer.update(clock.getDelta());
 	}
+}
 
+function resizeEvent() {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	
+	if (canvas.width != width || canvas.height != height)
+	{
+		canvas.width = width;
+		canvas.height = height;
+		
+		renderer = new THREE.WebGLRenderer({canvas});
+		renderer.setSize(width, height);
+
+		camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+		camera.rotation.y = 0.25 * Math.PI;
+		camera.position.x = 15;
+		camera.position.y = 15;
+		camera.position.z = 15;
+
+		controls = new THREE.OrbitControls(camera, renderer.domElement);
+		
+		repaint = true;
+	}
 }
